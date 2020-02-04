50304f8e9ca546d18f0892219c8d8af91c6282c3
Arvind kushwah <arvindkushwah9@gmail.com>
2020-02-04 13:37:26 +0530

    Add Migration

--- db/migrate/20191118054103_application_status_change_column_type.rb
+++ db/migrate/20191118054103_application_status_change_column_type.rb
@@ -1,5 +1,5 @@
 class ApplicationStatusChangeColumnType < ActiveRecord::Migration[5.2]
-  def change
-  	change_column(:applied_jobs, :application_status, :integer)
-  end
+  # def change
+  # 	change_column(:applied_jobs, :application_status, :integer)
+  # end
 end
